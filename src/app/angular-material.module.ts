2c1109519f396e1aa0d31219bfbdf688b6092753
philip993 <fitz993m@gmail.com>
2019-04-24 13:55:00 +0200

    AngularMaterial Module with imports

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import {
+  MatToolbarModule,
+  MatInputModule,
+  MatCardModule
+} from "@angular/material";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatToolbarModule, MatInputModule]
+})
+export class AngularMaterialModule {}
