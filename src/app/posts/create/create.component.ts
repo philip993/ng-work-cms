b4a6ccda9819ac08824fe7161184ad5b7e9d620e
philip993 <fitz993m@gmail.com>
2019-04-24 15:46:28 +0200

    Basic Reactive Form

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.css"]
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(""),
+      content: new FormControl("")
+    });
   }
 
+  onSubmit() {
+    console.log("submit");
+  }
 }
