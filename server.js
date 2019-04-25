4daf0a38c3fd0b9b25cdb19a520897343c23d7cc
philip993 <fitz993m@gmail.com>
2019-04-25 15:21:19 +0200

    CORS

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log("Couldn't connect to MongoDB"));
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, DELETE, PUT, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, X-Request-With, Authorization, Origin, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
